d34394eeb2db457b16e9362c33c50fe113b017ef
KamilSuchanek95 <34289315+KamilSuchanek95@users.noreply.github.com>
2018-07-04 11:14:47 +0200

    Add files via upload

--- model.c
+++ model.c
@@ -1,69 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <math.h>
 
-typedef struct
-{
-	/*Temperatura wewnętrzna*/
-	float Tw=25;
-	/*Temperatura zewnętrzna*/
-	float Tz=15;
-	/*Rezystancja cieplna ścian*/
-	float Rc=100;
-	/*Moc grzałki*/
-	float Pg=0.6;
-	/*Pojemność cieplna budynku*/
-	float Cc=0.003;
-	/*Aktywność grzałki*/
-	int G = 1;
-}model_st_t;
+/*Budynek posiada pojemność cieplną -> ilość ciepła potrzebna by zmienić temperaturę wewnątrz o stopień*/
+/*C [ J/K = (W*s)/K]*/
+/**/
+/*Moc grzałki jest stała, podlega załączeniu.*/
+/*Wyłączenie spowoduje spadek generowanego przezeń strumienia ciepła [J]*/
+/*Zależność opisująca spadek mocy grzałki o wartości początkowej P0:*/
+/*P(t) = P0 * exp(t/(R*C)) */
 
-model_st_t model_st;
-model_st_t* m=&model_st;
-
-void main()
-{
-	/*Prąd uchodzący*/
-	float Iout;
-	/*Prąd kondensatora*/
-	float Ic = 0;
-
+/*=====================================================================================================*/
+/*Parametry początkowe:*/
+typedef struct{
+	float Tw = 25;   /*Temperatura wewnętrzna budynku => [ *C ]*/
+	float Tz = -5;   /*Temperatura na zewnątrz budynku => [ *C ]*/
+	float C = 83150; /*Pojemność cieplna budynku => [ J/K = (W*s)/K ]*/
+	int G = 1;       /*Włączenie grzałki 0/1 => [ boolean ]*/
+	float Pg = 80000;/*Moc grzałki => [ W ]*/
+	float Pow = 400; /*Powierzchnia umowna budynku => [ m^2 ]*/
+	float U = 0.085; /*Przenikalność temperaturowa budynku => [ W/(m^2 * K) ]*/
+}model_st_t; model_st_t model_st; model_st_t* m=&model_st;
+/*=====================================================================================================*/
+void main(){
 	/*Utworzenie pliku na wyniki*/
 	FILE * fp;
 	fp = fopen("dane.txt", "w+");
 
-	m->G = PID_c(1);
+	/*Czas symulacji*/
+	int t[1000]; for(int i = 0; i<1000; i++){t[i]=i;}
+
+	/*Zależności:*/
+	typedef struct{
+	float dT = abs(abs(m->Tw) - abs(m->Tz));	 /*Różnica temperatur => [ K ]*/
+	float Ptot = m->U * m->Pow * (m->Tw - m->Tz);/*Strumień ciepła => [ W ]*/
+	float ZS = m->Pg / m->C; 					 /*Zysk/Strata liczone w stopniach => [ K = *C ]*/
+	float R = m->U^(-1);						 /*Opór temperaturowy budynku => [ (m^2 * K)/W ]*/
+	}model_wew_st_t; model_wew_st_t model_wew_st; model_wew_st_t* m_wew=&model_wew_st;
 
 	/*Symulacja*/
-	if(m->G)//
-	{
-	/*Napięcie jako różnica temperatur*/
-		float U = m->Tw - m->Tz;
-													/*Zwiększenie temperatury o ładunek kondensatora*/
-															m->Tw += m->Cc * U ;//Q*(1/dt)
-/*Prąd na zewnątrz - Uciekające ciepło*/
-		Iout = U / m->Rc;
-			/*Zapis do pliku*/
-		fprintf(fp,"%d\n", Iout);
+	float Pb = 1;
+	/*Pętla główna*/
+	for(int i = 0; i<1000; i++)/* t */{
+
+		/* G == 1 : (1) */
+		/* Ustalenie różnicy temperatur i ilości uciekającego ciepła */
+		/* Włączenie grzałki -> P(t) = Pb*exp(t/(R*C)) */
+		/* Nagrzewanie się grzałki */
+		/* Nagrzewanie budynku grzałką */
+		/* => (1)/(2) */
+		/*  */
+		/**/
+		/*  */
+		/* G != 1 : (2) */
+		/* Ustalenie różnicy temperatur i ilości uciekającego ciepła */
+		/* Wyłączenie grzałki -> P(t) = Pb*exp(-t/(R*C)) (???) */
+		/* Ochładzanie się grzałki */
+		/* Ochładzanie budynku */
+		/* =? (1)/(2) */
+
+	//if(m->G){fprintf(fp,"%d\n", 1);}else{fprintf(fp,"%d\n", 1);}
 	}
-	else
-	{
-	/*Napięcie jako różnica temperatur*/
-		float U = m->Tw - m->Tz;
-													/*Zmniejszenie temperatury pomieszczenia*/
-															m->Tw -= m->Cc * m->Rc * Ic;
-
-	/*Spadek prądu poprzez rozładowanie kondensatora*/
-		Ic = Ic * exp(1 / (m->Rc * m->Cc));
-		float Uc = m->Rc * Ic;
-		Iout = Uc / m->Rc;
-			/*Zapis do pliku*/
-		fprintf(fp,"%d\n", Iout);
-	}
-
-
 	fclose(fp);
 	//system("gnuplot rysowanie.gp -p");
 }
-/**/
+
+
